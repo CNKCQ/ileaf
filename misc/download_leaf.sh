e1a29a8fb211ee8fea382709ca72122e40f821e0
CNKCQ <wangchengqvan@gmail.com>
2022-08-12 11:05:01 +0800

    v0.3.1 lock the version
    v0.3.1 lock the version

--- misc/download_leaf.sh
+++ misc/download_leaf.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-
+# https://github.com/eycorsican/leaf/releases/latest/download/libleaf-ios.zip
 set -x
 
 WD=`pwd`
 
-curl -OL 'https://github.com/eycorsican/leaf/releases/latest/download/libleaf-ios.zip' \
+curl -OL 'https://github.com/eycorsican/leaf/releases/download/v0.3.1/libleaf-ios.zip' \
        && mv libleaf-ios.zip /tmp/ \
        && unzip -o /tmp/libleaf-ios.zip -d /tmp \
        && mv /tmp/libleaf.a PacketTunnel/libleaf/ \
